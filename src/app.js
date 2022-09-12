358e2c5ed5b709a114d48823d8ec9f6d5ad09aed
fkamag <fabiokamagian@gmail.com>
2022-09-12 14:23:10 -0300

    nova tentativa de corrigir o conflito em app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const productRouter = require('./routers/product.router');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use(express.json());
 app.use('/products', productRouter);
 
 // não remova essa exportação, é para o avaliador funcionar
