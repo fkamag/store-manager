89e6dfb68c218a570314a386584183aa85a16ed3
fkamag <fabiokamagian@gmail.com>
2022-09-13 23:21:31 -0300

    testes getAllProducts

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -2,18 +2,25 @@ const sinon = require('sinon');
 const chai = require('chai');
 const { productController } = require('../../../src/controllers');
 const { productService } = require('../../../src/services');
-const {
-  productsAllResponse,
-  productIdResponse,
-} = require('../../mocks/product.mock');
+const mock = require('../../mocks/product.mock');
 
 const { expect } = chai;
 
 describe('Verificar o Product Controller', () => {
 
   describe('Buscando todos os produtos', () => {
-    it('é chamado o status com o código 200', async () => {
-      sinon.stub(productService, 'getAllProducts').resolves(productsAllResponse);
+    it('retorna produtos com o status 200', async () => {
+      sinon.stub(productService, 'getAllProducts')
+        .resolves({ type: null, message: mock.productsAllResponse });
+      const req = {};
+      const res = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      const result = await productController.getAllProducts(req, res);
+      expect(res.status.calledWith(200)).to.be.equal(true);
+      expect(res.json.calledWith(mock.productsAllResponse)).to.be.equal(true);
+
+      afterEach(sinon.restore);
     });
 
     it('é chamado o json com a lista de motoristas', async function () {
--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -2,3 +2,15 @@ const sinon = require('sinon');
 const chai = require('chai');
 
 const { expect } = chai;
+
+const { productService } = require('../../../src/services');
+const { productModel } = require('../../../src/models');
+const mock = require('../../mocks/product.mock');
+
+describe('Verificar o Product Service', () => {
+  it('Buscando todos os produtos', async () => {
+    sinon.stub(productModel, 'getAllProducts').resolves(mock.productsAllResponse);
+    const result = await productService.getAllProducts();
+    expect(result).to.equal({ type: null, message: mock.productsAllResponse });
+  })
+})
